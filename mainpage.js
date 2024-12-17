7bc9d8dcb46af4add13cb769152094169daf32a8
Ron Myers <rem5329@gmail.com>
2024-12-17 10:26:14 -0600

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -5,15 +5,33 @@ function pageSelection(e) {
 //Functions:
 
 //generateFightMacro - takes list of monsters and adds their elements
+function generateFightMacro() {
+    //For each creature in the creature array, generate a line with their creature macro name
+}
 
 //generateCreatureMacro - takes a list of attacks/saves/damage/utility macros
+// Each ability goes in either Innate, Actions, Conditional Actions, Bonus Action, Reaction, or Utility
+// These categories should be split to be easy to see
+function generateCreatureMacro {
+    if (section is not empty) {
+      Display the section header, then loop through section to generate all the other sub-macros
+    }
+}
 
-//generateAttackMacro - Simple + to hit, and damage
-//generateAttackSaveMacro - + to hit, damage, save DC, and note on save
-//generateSaveMacro - Save DC, damage, and note on save
-//generateDamageMacro - only damage
-//generateUtilityMacro - non-damage, probably just a whisper
+//generateAttackMacro - Simple + to hit, and damage - atk
+//generateAttackNoDamageMacro - + to hit - npcatk
+//generateAttackSaveMacro - + to hit, damage, save DC, and note on save - atkdmg
+//generateSaveMacro - Save DC, damage, and note on save - dmg
+//generateDamageMacro - only damage - npcdmg
+//generateCheckMacro - for skill/attribute checks that might be needed - npcatk
+//generateSpellMacro - for spell descriptions - spell
+//generateUtilityMacro - non-damage - desc
 
-function writeToPage(completedMacro) {
-    document.getElementById("outputBlock").value = completedMacro
+//NOTE: Use the MancerRoll to generate random orders/players
+
+
+
+function writeToPage(macroStr) {
+    // Note: this should append to the end, not replace
+    document.getElementById("outputBlock").value = macroStr
 }
