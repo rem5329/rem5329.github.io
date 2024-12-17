81c2cce4561f84246821d65ad8b5d07cbaf2fabb
Ron Myers <rem5329@gmail.com>
2024-12-17 09:22:09 -0600

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -1,1 +1,19 @@
+function pageSelection(e) {
+    document.getElementById("myText").value = e.target.value
+}
 
+//Functions:
+
+//generateFightMacro - takes list of monsters and adds their elements
+
+//generateCreatureMacro - takes a list of attacks/saves/damage/utility macros
+
+//generateAttackMacro - Simple + to hit, and damage
+//generateAttackSaveMacro - + to hit, damage, save DC, and note on save
+//generateSaveMacro - Save DC, damage, and note on save
+//generateDamageMacro - only damage
+//generateUtilityMacro - non-damage, probably just a whisper
+
+function writeToPage(completedMacro) {
+    document.getElementById("outputBlock").value = completedMacro
+}
