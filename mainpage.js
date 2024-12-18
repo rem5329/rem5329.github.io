ad7a5c044a9865c6dd1d79499925f5c043ec54e6
Ron Myers <rem5329@gmail.com>
2024-12-18 09:25:45 -0600

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -239,10 +239,6 @@ function generateAbilityMacro(type) {
 
 //NOTE: Use the MancerRoll to generate random orders/players
 
-
-
-
-*/
 function appendToOutput(str) {
     document.getElementById("outputBlock").innerHTML += str;
 }
